85a347e241de57d28af9efe248779fc38cc94e6f
BrukeYd <bruke0077.08@gmail.com>
2023-09-26 21:37:48 +0300

    modified little menu and add javascript files

---
+++ js/btn.js
@@ -0,0 +1,16 @@
+function mouseOver(){
+    alert('working');
+}
+var windows = document.documentElement.clientWidth ; 
+
+function onResize()
+{
+    if(document.documentElement.clientWidth < 500)
+    {
+        alert('windows is minimized');
+    }
+    else
+    {
+        alert('it good to go');
+    }
+}
---
+++ js/events.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("resize", myFunction);
+function myFunction(){
+ 
+   var littleMenu = document.getElementsByClassName("lmenu");
+   littleMenu.setAttribute("style", "display: none;");
+};
