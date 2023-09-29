a830cf59625b1ee24f523df9532a01827000f16c
BrukeYd <bruke0077.08@gmail.com>
2023-09-29 19:23:59 +0300

    menu button work out and menus editions

--- js/events.js
+++ js/events.js
@@ -4,13 +4,16 @@ function myFunction(){
  
    var header = document.getElementsByTagName("header");
    var div = header[0].getElementsByTagName("div");
-   var drpIcon = div[2].getElementsByTagName("img");
+   var drpIcon = div[3].getElementsByTagName("img");
    var width = document.documentElement.offsetWidth;
    
    if(width < 1200)
    {
       div[0].setAttribute("style", "display: none;");
       div[2].setAttribute("style", "display: none;");
+      drpIcon[0].setAttribute("style","height:60px;");
+      header[0].setAttribute("style", "height:90px;")
+      div[1].setAttribute("style","margin-top:20px; margin-right:15px;")
    }
    else
    {
